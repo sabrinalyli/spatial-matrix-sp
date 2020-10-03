932008a4f3fe6b98ace73d6fee47cb6532ce0b65
sabrinalyli <69806384+sabrinalyli@users.noreply.github.com>
2020-10-03 16:54:56 +0100

    Update adjacency-matrix-map.R

--- adjacency-matrix-map.R
+++ adjacency-matrix-map.R
@@ -1,9 +1,13 @@
+#this code this plots the adjacency matrix used in our spatio-temporal model on a map to show the network connections between municipalities based on (1) mobility data and (2) nearest neighbour 
+
 library(dplyr)
 library(network)
 library(maps)
 library(sf)
 
-mob<-readxl::read_excel("mat_bin_125.xlsx")
+#import mobility matrix with linked municipality codes
+#these have been uploaded on dropbox in the mobility network folder
+mob<-readxl::read_excel("mat_bin_175.xlsx")
 
 #convert to dataframe
 mat <- as.data.frame(mob)
@@ -27,8 +31,6 @@ gmsp_df<-subset(muni_sp,code_muni %in% metro_sp_munis)
 mob_gmsp<-subset(mob_df, start %in% metro_sp_munis & end %in% metro_sp_munis ) %>%
   glimpse()
 
-#mob_mat<-matrix(sapply(mob_gmsp$trips, as.numeric), nrow=39,byrow = TRUE) 
-
 #match coordinates with network data
 #first we need to calculate lat/long of centroids of shapefile  
 gmsp_df$centroids <- st_transform(gmsp_df, 29101) %>% 
@@ -38,6 +40,7 @@ gmsp_df$centroids <- st_transform(gmsp_df, 29101) %>%
   # since you want the centroids in a second geometry col:
   st_geometry()
 
+#match for sp state
 muni_sp$centroids <- st_transform(muni_sp, 29101) %>% 
   st_centroid() %>% 
   # this is the crs from d, which has no EPSG code:
@@ -57,37 +60,34 @@ gmsp_coord<-st_coordinates(gmsp_df$centroids)
 
 sp_coord_cent<-st_coordinates(muni_sp$centroids) 
 
-#create  two df, one containing the lat/long of start location and another for the end location
-gmsp_coord_start<-cbind(gmsp_coord,gmsp_df[,c("code_muni")])  %>%
+#create  a df that contains the municipality codes and coordinates which will be used for merging
+gmsp_coord_merge<-cbind(gmsp_coord,gmsp_df[,c("code_muni")])  %>%
   mutate(start=code_muni) %>%
   mutate(start=as.character(start)) %>%
+  mutate(end=code_muni) %>%
+  mutate(end=as.character(end)) %>%
   select(-code_muni) %>%
   glimpse()
 
-gmsp_coord_end<-cbind(gmsp_coord,gmsp_df[,c("code_muni")])  %>%
-  mutate(end=code_muni) %>%
-  mutate(end=as.character(end)) %>%
-  glimpse()
-
-#now merge these coordinate columns with the trip data
-coord_start<- left_join(mob_gmsp,gmsp_coord_start[,c("X","Y","start")],by="start") %>%
+#now merge these coordinate columns with the trip data using "start" and "end" locations as identifiers
+coord_start<- left_join(mob_gmsp,gmsp_coord_merge[,c("X","Y","start")],by="start") %>%
   mutate(lon.start=X,lat.start=Y) %>%
   select(-geom,-X,-Y) %>%
   glimpse()
 
-coord_end<- left_join(mob_gmsp,gmsp_coord_end[,c("X","Y","end")],by="end") %>%
+coord_end<- left_join(mob_gmsp,gmsp_coord_merge[,c("X","Y","end")],by="end") %>%
   mutate(lon.end=X,lat.end=Y) %>%
   select(-geom,-X,-Y) %>%
   glimpse()
 
-#create final df containing start lat-long and end lat-long
+#create final df containing both the lat/long of origin and destination locations 
 coord_gmsp<-left_join(coord_start,coord_end[,c("lon.end","lat.end","end","start")],
                       by=c("start","end")) %>%
   filter(trips==1) %>%
   glimpse()
 
 
-#sp
+#do this for sp state now
 sp_coord_merge<-cbind(sp_coord_cent,muni_sp[,c("code_muni")])  %>%
   mutate(start=code_muni) %>%
   mutate(start=as.character(start)) %>%
@@ -96,7 +96,6 @@ sp_coord_merge<-cbind(sp_coord_cent,muni_sp[,c("code_muni")])  %>%
   select(-code_muni) %>%
   glimpse()
 
-#now merge these coordinate columns with the trip data
 sp_coord_start<- left_join(mob_df,sp_coord_merge[,c("X","Y","start")],by="start") %>%
   mutate(lon.start=X,lat.start=Y) %>%
   select(-geom,-X,-Y) %>%
@@ -107,7 +106,6 @@ sp_coord_end<- left_join(mob_df,sp_coord_merge[,c("X","Y","end")],by="end") %>%
   select(-geom,-X,-Y) %>%
   glimpse()
 
-#create final df containing start lat-long and end lat-long
 coord_sp<-left_join(sp_coord_start,sp_coord_end[,c("lon.end","lat.end","end","start")],
                       by=c("start","end")) %>%
   filter(trips==1) %>%
@@ -115,15 +113,15 @@ coord_sp<-left_join(sp_coord_start,sp_coord_end[,c("lon.end","lat.end","end","st
 
 #plot mobility network on map 
 network_plot_mob<- ggplot() +
-  geom_sf(data =  gmsp_df, #muni_sp,
+  geom_sf(data =  muni_sp, #muni_sp,  gmsp_df
                show.legend = FALSE,alpha = 0.25,
                color = "grey") +
-  geom_segment(data = coord_gmsp, #coord_sp, 
+  geom_segment(data =  coord_sp, #coord_sp, coord_gmsp
                aes(x = lon.start, xend = lon.end,
                    y = lat.start, yend = lat.end),
                color="red", 
                size = 0.25,alpha = 0.5) +
-  geom_point(data = data.frame(sp_coord_cent), #gmsp_coord
+  geom_point(data = data.frame(sp_coord_cent), #sp_coord_cent gmsp_coord
              aes(X, Y), color = "black",shape=1) +
   theme_bw() +
   theme(axis.line = element_line(),
@@ -135,19 +133,21 @@ network_plot_mob<- ggplot() +
         axis.text = element_blank(),
         ) +
   labs(title = "Municipality network as defined in the adjacency matrix",
-       subtitle="Threshold = 150 trips")
+       subtitle="Threshold = 175 trips")  #change this on the threshold you are plotting
 
 network_plot_mob
 
 #====now plot network based on nearest neighbour adjacency matrix====
-#first convert list (nb) into a matrix
 
+#first convert list (nb) into a matrix
 num.el <- sapply(nb, length)
+
 # Generate the neighbouring dataframe
 res <- as.data.frame(cbind(unlist(nb), rep(1:length(nb), num.el))) %>%
   rename(muni.start=V1,muni.end=V2) 
 
 #now we need to link location id with code_muni
+#first create a df with the municipality codes that we need to do the linking
 muni_sp_link <- muni_sp %>%
   select(code_muni) %>%
   mutate(muni.start=1:nrow(.)) %>%
@@ -157,17 +157,17 @@ muni_sp_link <- muni_sp %>%
   select(-code_muni) %>%
   glimpse()
 
+#link data to create a df that contains the nearest neighbour connections and the municipality codes of the start and end locations 
 muni_sp_nb<-left_join(res,muni_sp_link[c("start","muni.start")],by="muni.start") %>%
   left_join(muni_sp_link[,c("end","muni.end")],by="muni.end") %>%
   select(-geom.x,-geom.y) %>%
   glimpse()
 
-
 #filter out munis from gmsp
 nb_gmsp<-subset(muni_sp_nb, start %in% metro_sp_munis & end %in% metro_sp_munis ) %>%
   glimpse()
 
-#now add coordinates
+#now add coordinates for both start and end locations 
 coord_start_nb<- left_join(nb_gmsp,gmsp_coord_start[,c("X","Y","start")],by="start") %>%
   mutate(lon.start=X,lat.start=Y) %>%
   select(-geom,-X,-Y) %>%
@@ -178,38 +178,35 @@ coord_end_nb<- left_join(nb_gmsp,gmsp_coord_end[,c("X","Y","end")],by="end") %>%
   select(-geom,-X,-Y) %>%
   glimpse()
 
-#create final df containing start lat-long and end lat-long
 coord_gmsp_nb<-left_join(coord_start_nb,coord_end_nb
                          [,c("lon.end","lat.end","end","start")],
                       by=c("start","end")) %>%
   glimpse()
 
-#sp
+#do the above for sp 
 coord_start_nb_sp<- left_join(muni_sp_nb,sp_coord_start[,c("lon.start","lat.start","start","end")],by=c("start","end")) %>%
   glimpse()
 
 coord_end_nb_sp<- left_join(muni_sp_nb,sp_coord_end[,c("lon.end","lat.end","start","end")],by=c("start","end")) %>%
   glimpse()
 
-#create final df containing start lat-long and end lat-long
 coord_sp_nb<-left_join(coord_start_nb_sp,coord_end_nb_sp
                          [,c("lon.end","lat.end","end","start")],
                          by=c("start","end")) %>%
   glimpse()
 
 
-#plot
 #plot mobility network on map 
 network_plot_nb<- ggplot() +
-  geom_sf(data = muni_sp, #gmsp_df
+  geom_sf(data = gmsp_df, #muni_sp
           show.legend = FALSE,alpha = 0.25,
           color = "grey") +
-  geom_segment(data = coord_sp_nb, #coord_gmsp_nb
+  geom_segment(data =coord_gmsp_nb, #coord_sp_nb
                aes(x = lon.start, xend = lon.end,
                    y = lat.start, yend = lat.end),
                color="red", 
                size = 0.25,alpha = 0.5) +
-  geom_point(data = data.frame(sp_coord_cent), #gmsp_coord
+  geom_point(data = data.frame(gmsp_coord), #sp_coord_cent
              aes(X, Y), color = "black",shape=1) +
   theme_bw() +
   theme(axis.line = element_line(),
